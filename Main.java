69e43efffb6655564fee5c9fc47af4d9dc2226e5
MonkeyNoChunky <oweefo@gmail.com>
2025-10-17 10:50:02 -0400

    added barebones selectionsort

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int[] array = new int[50];
+    }
+
+    static void selectionSort(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            for(int o = i + 1; o < array.length; o++) {
+                int lowestNum = array[i];
+                if(array[o] < lowestNum) {
+                    lowestNum = array[o];
+                }
+            }
+            swap(array[i], lowestNum);
+        }
     }
 }
