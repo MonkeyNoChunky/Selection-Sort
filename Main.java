615a15d80b33f58ddec3afbcf0e533a4e77f3414
MonkeyNoChunky <oweefo@gmail.com>
2025-10-17 11:37:05 -0400

    not wokring selection sort

--- Main.java
+++ Main.java
@@ -2,69 +2,49 @@ import java.util.Scanner;
 
 
 public class Main {
-
-
-       static int[] array; 
     public static void main(String[] args) {
-
-        System.out.println("Please input number of values");
-
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Please input number of values");
         int arrayLength = scan.nextInt(); 
-
-        array = new int[arrayLength];
-
+        int[] array = new int[arrayLength];
 
         for (int i = 0; i < arrayLength; i++) {
-            
-            System.out.println("Please insert your " + i + " value"); 
-
+            System.out.println("Please insert your " + (i + 1) + " value"); 
             array[i] = scan.nextInt(); 
-
         }
 
-
-        System.out.println("Unsorted array "  + array.toString()); 
-
+        printArray(array);
         selectionSort(array);
-
-
-        System.out.println("Sorted Array: " + array.toString()); 
-
+        System.out.println();
+        printArray(array); 
     }
 
 
-    public static void swap(int arrayValue, int lowestNum, int lowestNumberPosition, int currentPosition) {
-
-        int placeHolder = arrayValue;
-
-        array[lowestNumberPosition] = lowestNum; 
-
-        array[currentPosition] = placeHolder; 
-
-
-
-
+    public static void swap(int index1, int index2, int[] array) {
+        int placeHolder = array[index1];
+        array[index1] = array[index2]; 
+        array[2] = placeHolder; 
     }
 
 
     static void selectionSort(int[] array) {
         int lowestNum = 0; 
-        int currentPosition = 0; 
-        for(int i = 0; i < array.length; i++) {
-            for(int o = i + 1; o < array.length - 1; o++) {
-                currentPosition = o;
+        int lowestNumIndex = 0; 
+        for(int i = 0; i < array.length - 1; i++) {
+            for(int o = i + 1; o < array.length; o++) {
+                lowestNumIndex = o;
                 lowestNum = array[i];
                 if(array[o] < lowestNum) {
                     lowestNum = array[o];
                 }
             }
-            swap(array[i], lowestNum, i, currentPosition);
+            swap(i, lowestNumIndex, array);
         }
     }
 
-
-    
-
+    static void printArray(int[] array) {
+        for(int i : array) {
+            System.out.print(i + " ");
+        }
+    }
 }
